4886e3d81fd26e813596ca25707ba7118a339384
Oleksandr <sasha.sochka@gmail.com>
2013-10-10 20:32:13 +0300

    Add default custom-settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,3 +17,15 @@
 (setenv "MAILHOST" "pop.gmail.com")
 (setq rmail-primary-inbox-list '("op:sasha.sochka")
       rmail-pop-password-required t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
