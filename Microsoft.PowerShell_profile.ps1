05d427d02aac9d6d4f4fdbdc9be436fb3c476e16
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-05-02 02:06:37 +0300

    a bunch of powershell config changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,36 @@
+function ff ([string] $glob) { get-childitem -recurse -include $glob }
+
+function osr { shutdown -r -t 5 }
+
+function osh { shutdown -h -t 5 }
+
+function rmd ([string] $glob) { remove-item -recurse -force $glob }
+
+function whoami { (get-content env:\userdomain) + "\" + (get-content env:\username); }
+
+function strip-extension ([string] $filename)
+{
+    [system.io.path]::getfilenamewithoutextension($filename)
+}
+
+function New-PSSecureRemoteSession
+{
+	param ($sshServerName, $Cred)
+	$Session = New-PSSession $sshServerName -UseSSL -Credential $Cred -ConfigurationName C2Remote
+	Enter-PSSession -Session $Session
+}
+
+function New-PSRemoteSession
+{
+	param ($shServerName, $Cred)
+	$shSession = New-PSSession $shServerName -Credential $Cred -ConfigurationName C2Remote
+	Enter-PSSession -Session $shSession
+}
+
+function gotoprojects{set-location E:\projects}
+function gotoprofile{set-location C:\Users\Oleksandr\Documents\WindowsPowerShell}
+function gotodownloads{set-location C:\Users\Oleksandr\Downloads}
+
 function Enum-Lines($filename) {
    $content = gc $filename
 
@@ -12,8 +45,11 @@ function Enum-Lines($filename) {
 
 set-alias nl Enum-Lines
 set-alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
+set-alias grep select-string
 set-alias ssh New-PSSecureRemoteSession
 set-alias sh New-PSRemoteSession
+set-alias profile gotoprofile
+set-alias downloads gotodownloads
 set-alias cwd Get-Location
 function .. {
 cd ..;
